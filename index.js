8e7b4ab3b671738bafe173cb3f46d3e5e5c50e80
ana <anameciarobalo@gmail.com>
2020-01-05 21:08:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution here!
+// Write your solution her
+
+const extra = ["Odie"]
+
+const append = ["Milo", "Otis", "Garfield", ...extra]
+
+const prepend = [...extra, "Milo", "Otis", "Garfield"]
+
+const removeLast = ["Milo", "Otis", "Garfield"].splice(0,2)
+
+const removeFirst = ["Milo", "Otis", "Garfield"].splice(1)
+
+
+
+
